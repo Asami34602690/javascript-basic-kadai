53c620ea138e378e872421aa25c28d0e6d6590d9
Asami34602690 <155605633+Asami34602690@users.noreply.github.com>
2024-02-03 10:06:21 +0900

    kadai_20アップロード

---
+++ kadai_020/event.js
@@ -0,0 +1,7 @@
+//クリックしたときにイベント処理を実行する//
+window.addEventListener('click', () => {
+    //ボタンをクリックしましたを表示する//
+    const text = document.getElementById('text');
+    text.textContent  = "ボタンをクリックしました";
+    
+});
